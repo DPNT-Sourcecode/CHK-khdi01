30928cef528c0cd9006a2de1a5e51d364fd108b9
sbx_user1051 <sbx_user1051@169.254.12.33>
2025-02-24 19:03:04 +0000

    Mon Feb 24 19:03:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,40 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    prices = {"A": 50, "B":30, "C":20, "D":15}
+    offers = {"A": (3, 130), "B": (2,45)}
+
+
+    if skus[0].isnumeric():
+        return -1
+
+    current_number = ""
+
+    item_list = []
+    for i in skus.upper():
+        if i in prices.keys():
+            if current_number != "":
+                item_list.append(current_number)
+                current_number = ""
+            item_list.append(i)
+        else:
+            current_number += i
+    
+    items = item_list[::2]
+    quantities = item_list[1::2]
+
+    all_quantites_are_numbers_check = [i.isnumeric() for i in quantites]
+
+    if (len(items) != len(quantites)) or False in all_quantites_are_numbers_check:
+        return -1
+
+    total = 0
+
+    for item, n in zip(items,quantities):
+        if item in offers.keys():
+            total += (n // offers[item][0] * offers[item][1]) + (n % offers[item][0] * prices[item])
+        else:
+            total += (n * prices[item])
+
+    return total
+
